068a3bee982ecf9472c076895c476c7af86f7d5e
Minne Sandstra <90246588+minnesandstra@users.noreply.github.com>
2026-09-03 16:10:49 +0200

    Add backend config

---
+++ config.js
@@ -0,0 +1,5 @@
+// Na het deployen van Google Apps Script vervang je onderstaande waarde.
+// Voorbeeld: https://script.google.com/macros/s/AKfycb.../exec
+window.APP_CONFIG = {
+  API_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE"
+};
